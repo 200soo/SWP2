b88ed29a4a06dccd8d77ce2d8fddc93e57b28e29
koomchang <90228925+koomchang@users.noreply.github.com>
2021-10-09 20:56:51 +0900

    최종본

--- 20181637/assignment3_20181637.py
+++ 20181637/assignment3_20181637.py
@@ -33,39 +33,34 @@ def doScoreDB(scdb):
         if inputstr == "": continue
         parse = inputstr.split(" ")
         if parse[0] == 'add':
-            record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3]}
+            record = {'Name' : parse[1], 'Age' : parse[2], 'Score' : parse[3]}
             scdb += [record]
-        elif parse[0] == 'del': #break 지웠음 / del "이름잘못입력" 했을때 예외처리 안됌
+        elif parse[0] == 'del':
             try:
-                for p in scdb:
-                    if p['Name'] == parse[1]:
-                        scdb.remove(p)
+                scdb = [p for p in scdb if p['Name'] != parse[1]]
             except IndexError:
                 print("del \"Name\" 형태로 입력하시오.")
         elif parse[0] == 'show':
             sortKey ='Name' if len(parse) == 1 else parse[1]
             showScoreDB(scdb, sortKey)
-        elif parse[0] == 'find': #add 하고 find을 하면 안 찾아짐 ㅠㅠ
+        elif parse[0] == 'find':
             try:
                 for p in scdb:
                     if p['Name'] == parse[1]:
                         for attr in sorted(p):
                             print(attr + "=" + p[attr], end=' ')
                         print()
-                        break
-                    else:
-                        print("없는 이름입니다.")
-                        break
             except IndexError as e:
                 print("find \"Name\" 형태로 입력하시오.")
-        elif parse[0] == 'inc': #예외처리 실패 ㅠㅠ (인덱스가 아무것도 없을때는 except 넘어가지만 인덱스 하나라도 있으면 오류 x)
+        elif parse[0] == 'inc':
             try:
                 for p in scdb:
-                    if p['Name'] == parse[1]:
-                        a = int(p['Score'])
-                        a += int(parse[2])
-                        p['Score'] = str(a)
-            except IndexError as e:\
+                    if int(parse[2]) > 0:
+                        if p['Name'] == parse[1]:
+                            a = int(p['Score'])
+                            a += int(parse[2])
+                            p['Score'] = str(a)
+            except IndexError as e:
                 print("parse \"Name\" \"Score\" 형태로 입력하시오.")
         elif parse[0] == 'quit':
             break

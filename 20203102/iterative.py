395a9455752af38e0596852bba6729d8e4397014
WSH <66334196+200soo@users.noreply.github.com>
2021-10-12 09:32:38 +0900

    Create iterative.py

---
+++ 20203102/iterative.py
@@ -0,0 +1,29 @@
+import time
+import random
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+def iterfibo(n):
+    num1 = 1
+    num2 = 1
+    if n <= 1:
+        return n
+    for i in range(n-2):
+        num1, num2 = num2, num1 + num2
+    return num2
+
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" % (nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))

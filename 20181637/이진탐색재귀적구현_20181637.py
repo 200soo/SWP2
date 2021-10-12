3f4375b63bc122b69d9ad7b0bc5c79d1a37f8cbb
koomchang <90228925+koomchang@users.noreply.github.com>
2021-10-12 11:29:33 +0900

    무한루프 걸린 거 같습니다

---
+++ 20181637/이진탐색재귀적구현_20181637.py
@@ -0,0 +1,59 @@
+import time
+import random
+
+
+def seqsearch(nbrs, target):
+    for i in range(0, len(nbrs)):
+        if (target == nbrs[i]):
+            return i
+    return -1
+
+
+def recbinsearch(L, l, u, target):
+    idx = 0
+    while (l <= u):
+        middle = int((l + u) // 2)
+        if L[middle] == target :
+            idx = middle
+            break
+        elif L[middle] < target :
+            recbinsearch(L, middle + 1, u, target)
+        else:
+            recbinsearch(L, l, middle - 1, target)
+    return idx
+
+
+numofnbrs = int(input("Enter a number: "))
+numbers = []
+for i in range(numofnbrs):
+    numbers += [random.randint(0, 999999)]
+
+numbers = sorted(numbers)
+
+numoftargets = int(input("Enter the number of targets: "))
+targets = []
+for i in range(numoftargets):
+    targets += [random.randint(0, 999999)]
+
+
+ts = time.time()
+
+# binary search - recursive
+cnt = 0
+for target in targets:
+    idx = recbinsearch(numbers, 0, len(numbers), target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("recbinsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))
+
+ts = time.time()
+
+# sequential search
+cnt = 0
+for target in targets:
+    idx = seqsearch(numbers, target)
+    if idx == -1:
+        cnt += 1
+ts = time.time() - ts
+print("seqsearch %d: not found %d time %.6f" % (numoftargets, cnt, ts))

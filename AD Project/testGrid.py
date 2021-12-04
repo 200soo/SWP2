0425fc1b689b3b8a1f1354569c2365d98587e4d0
subin <61944367+SamSingbong@users.noreply.github.com>
2021-12-04 12:11:06 +0900

    Update testGrid.py

--- AD Project/testGrid.py
+++ AD Project/testGrid.py
@@ -6,16 +6,29 @@ grid = Grid()
 
 # 그리드 생성되는 것 확인
 grid.createGrid()
-print(grid.originGrid)
-
+for row in range(0, 9):
+    res = ''
+    for col in range(0, 9):
+        res += str(grid.originGrid[row][col]) + ' '
+    print(res)
 print('\n')
 
 # 어려움 모드일 때 blank 생성 확인
+print('difficult')
 grid.createBlank(1)
-print(grid.blankGrid)
-
+for row in range(0, 9):
+    res = ''
+    for col in range(0, 9):
+        res += str(grid.blankGrid[row][col]) + ' '
+    print(res)
 print('\n')
 
 # 쉬움 모드일 때 blank 생성 확인
+print('easy')
 grid.createBlank(2)
-print(grid.blankGrid)
+for row in range(0, 9):
+    res = ''
+    for col in range(0, 9):
+        res += str(grid.blankGrid[row][col]) + ' '
+    print(res)
+print('\n')
